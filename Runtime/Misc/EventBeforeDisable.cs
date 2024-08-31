e86f112e78104f0c281f956d0607d7bbe9c4d561
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2024-08-31 14:57:58 +0200

    Added EventBeforeDisable

---
+++ Runtime/Misc/EventBeforeDisable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Rehawk.Foundation.Misc
+{
+    [DisallowMultipleComponent]
+    public class EventBeforeDisable : MonoBehaviour
+    {
+        public event Action<EventBeforeDisable> BeforeDisabled;
+        
+        private void OnDisable()
+        {
+            BeforeDisabled?.Invoke(this);
+        }
+    }
+}

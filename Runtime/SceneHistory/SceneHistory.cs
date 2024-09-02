f00f9d92c956621581cc6ecfa31eba233ff28ad5
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2024-09-02 11:11:24 +0200

    Added scene history

---
+++ Runtime/SceneHistory/SceneHistory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Rehawk.Foundation.Extensions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Rehawk.Foundation.SceneHistory
+{
+    public static class SceneHistory
+    {
+        private static readonly List<SceneHistoryRecord> history = new List<SceneHistoryRecord>();
+
+        static SceneHistory()
+        {
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+        }
+
+        public static SceneHistoryRecord GetPrevious()
+        {
+            return Get(1);
+        }
+        
+        public static SceneHistoryRecord Get(int stepsBack)
+        {
+            int index = history.Count - 1 - stepsBack;
+            if (history.TryGetValue(index, out SceneHistoryRecord record))
+            {
+                return record;
+            }
+
+            return SceneHistoryRecord.Empty;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void OnSubsystemRegistration()
+        {
+            history.Clear();
+        }
+
+        private static void OnActiveSceneChanged(Scene previousScene, Scene newScene)
+        {
+            history.Add(new SceneHistoryRecord
+            {
+                SceneName = newScene.name,
+                BuildIndex = newScene.buildIndex
+            });
+        }
+    }
+}
---
+++ Runtime/SceneHistory/SceneHistoryRecord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Rehawk.Foundation.SceneHistory
+{
+    public struct SceneHistoryRecord : IEquatable<SceneHistoryRecord>
+    {
+        public string SceneName { get; set; }
+        public int BuildIndex { get; set; }
+
+        public static SceneHistoryRecord Empty
+        {
+            get
+            {
+                return new SceneHistoryRecord
+                {
+                    BuildIndex = -1
+                };
+            }
+        }
+        
+        public bool Equals(SceneHistoryRecord other)
+        {
+            return SceneName == other.SceneName && BuildIndex == other.BuildIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SceneHistoryRecord other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((SceneName != null ? SceneName.GetHashCode() : 0) * 397) ^ BuildIndex;
+            }
+        }
+        
+        public static bool operator ==(SceneHistoryRecord recordA, SceneHistoryRecord recordB)
+        {
+            return Equals(recordA, recordB);
+        }
+        
+        public static bool operator !=(SceneHistoryRecord recordA, SceneHistoryRecord recordB)
+        {
+            return !Equals(recordA, recordB);
+        }
+    }
+}

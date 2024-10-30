1e5862603de9184ac28fe9cf5437e848e14e8947
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2024-10-30 11:49:40 +0100

    Improved post processor utility

--- Editor/Assets/AssetPostprocessorUtility.cs
+++ Editor/Assets/AssetPostprocessorUtility.cs
@@ -11,158 +11,134 @@ namespace Rehawk.Foundation.Assets
 {
     public static class AssetPostprocessorUtility
     {
-        public static void MakeScriptableObjectIdUnique<T>(string[] importedAssets, string idFieldName) where T : ScriptableObject
+        public static void MakeScriptableObjectIdUnique<T>(string[] importedAssets, string uidFieldName) where T : ScriptableObject
         {
             Type type = typeof(T);
             
-            string projectPath = Path.GetDirectoryName(Application.dataPath);
-
-            FieldInfo guidFieldInfo = type.GetField(idFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-
-            T[] objects = AssetHelper.FindAssetsOfType<T>();
+            T[] allObjects = AssetHelper.FindAssetsOfType<T>();
 
             for (int i = 0; i < importedAssets.Length; i++)
             {
                 if (type.IsAssignableFrom(AssetDatabase.GetMainAssetTypeAtPath(importedAssets[i])))
                 {
                     var importedObject = AssetDatabase.LoadAssetAtPath<T>(importedAssets[i]);
-
-                    string importedGuid = guidFieldInfo.GetValue(importedObject)?.ToString();
-
-                    if (string.IsNullOrEmpty(importedGuid))
-                    {
-                        guidFieldInfo.SetValue(importedObject, Guid.NewGuid().ToString());
-                        EditorUtility.SetDirty(importedObject);
-                    }
-                    else
-                    {
-                        T objectWithSameGuid = objects.FirstOrDefault(obj =>
-                        {
-                            if (obj != importedObject)
-                            {
-                                object guid = guidFieldInfo.GetValue(obj);
-
-                                return guid.Equals(importedGuid);
-                            }
-                            
-                            return false;
-                        });
-                        
-                        if (objectWithSameGuid != null)
-                        {
-                            var importedCreationTime = File.GetCreationTimeUtc(importedAssets[i]);
-
-                            string pathToLoadedObject = Path.Combine(projectPath, AssetDatabase.GetAssetPath(objectWithSameGuid));
-                            
-                            var loadedCreationTime = File.GetCreationTimeUtc(pathToLoadedObject);
-
-                            if (importedCreationTime >= loadedCreationTime)
-                            {
-                                guidFieldInfo.SetValue(importedObject, Guid.NewGuid().ToString());
-                                EditorUtility.SetDirty(importedObject);
-                            }
-                            else
-                            {
-                                guidFieldInfo.SetValue(importedObject, Guid.NewGuid().ToString());
-                                EditorUtility.SetDirty(objectWithSameGuid);
-                            }
-                        }
-                    }
+                    
+                    MakeIdUnique(importedAssets[i], importedObject, uidFieldName, allObjects);
                 }
             }
         }
         
-        public static void MakeComponentIdUnique<T>(string[] importedAssets, string idFieldName, IReadOnlyList<T> allComponents) where T : MonoBehaviour
+        public static void MakeComponentIdUnique<T>(string[] importedAssets, string uidFieldName, IReadOnlyList<T> allComponents) where T : MonoBehaviour
         {
-            Type type = typeof(T);
-
-            string projectPath = Path.GetDirectoryName(Application.dataPath);
-
-            FieldInfo guidFieldInfo = type.GetField(idFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-
             for (int i = 0; i < importedAssets.Length; i++)
             {
                 var componentObject = AssetDatabase.LoadAssetAtPath<GameObject>(importedAssets[i]);
 
                 if (componentObject && componentObject.TryGetComponent(out T importedComponent))
                 {
-                    string importedGuid = guidFieldInfo.GetValue(importedComponent)?.ToString();
+                    MakeIdUnique(importedAssets[i], importedComponent, uidFieldName, allComponents);
+                }
+            }
+        }
 
-                    if (string.IsNullOrEmpty(importedGuid))
+        private static void MakeIdUnique<T>(string importedAssetPath, T importedObject, string uidFieldName, IReadOnlyList<T> allObjects) 
+            where T : Object
+        {
+            Type type = typeof(T);
+
+            FieldInfo uidFieldInfo = type.GetField(uidFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+            string projectPath = Path.GetDirectoryName(Application.dataPath);
+            string importedGuid = uidFieldInfo.GetValue(importedObject)?.ToString();
+
+            if (string.IsNullOrEmpty(importedGuid))
+            {
+                uidFieldInfo.SetValue(importedObject, Guid.NewGuid().ToString());
+                EditorUtility.SetDirty(importedObject);
+            }
+            else
+            {
+                T objectWithSameGuid = allObjects.FirstOrDefault(component =>
+                {
+                    if (component != importedObject)
                     {
-                        guidFieldInfo.SetValue(importedComponent, Guid.NewGuid().ToString());
-                        EditorUtility.SetDirty(componentObject);
+                        object guid = uidFieldInfo.GetValue(component);
+                        return guid.Equals(importedGuid);
+                    }
+                        
+                    return false;
+                });
+                    
+                if (objectWithSameGuid != null)
+                {
+                    var importedCreationTime = File.GetCreationTimeUtc(importedAssetPath);
+
+                    string objectWithSameGuidPath = Path.Combine(projectPath, AssetDatabase.GetAssetPath(objectWithSameGuid));
+                        
+                    DateTime objectWithSameGuidCreationTime = File.GetCreationTimeUtc(objectWithSameGuidPath);
+
+                    if (importedCreationTime >= objectWithSameGuidCreationTime && 
+                        (!PrefabUtility.IsPartOfPrefabAsset(importedObject) || ((PrefabUtility.IsPartOfRegularPrefab(importedObject) && PrefabUtility.IsPartOfRegularPrefab(objectWithSameGuid)) || (PrefabUtility.IsPartOfVariantPrefab(importedObject) && PrefabUtility.IsPartOfRegularPrefab(objectWithSameGuid))) || (PrefabUtility.IsPartOfVariantPrefab(importedObject) && PrefabUtility.IsPartOfVariantPrefab(objectWithSameGuid))))
+                    {
+                        uidFieldInfo.SetValue(importedObject, Guid.NewGuid().ToString());
+                        EditorUtility.SetDirty(importedObject);
                     }
                     else
                     {
-                        T componentWithSameGuid = allComponents.FirstOrDefault(component =>
-                        {
-                            if (component != importedComponent)
-                            {
-                                object guid = guidFieldInfo.GetValue(component);
-
-                                return guid.Equals(importedGuid);
-                            }
-                        
-                            return false;
-                        });
-                    
-                        if (componentWithSameGuid != null)
-                        {
-                            var importedCreationTime = File.GetCreationTimeUtc(importedAssets[i]);
-
-                            string pathToLoadedObject = Path.Combine(projectPath, AssetDatabase.GetAssetPath(componentWithSameGuid));
-                        
-                            var loadedCreationTime = File.GetCreationTimeUtc(pathToLoadedObject);
-
-                            if (importedCreationTime >= loadedCreationTime)
-                            {
-                                guidFieldInfo.SetValue(importedComponent, Guid.NewGuid().ToString());
-                                EditorUtility.SetDirty(importedComponent);
-                            }
-                            else
-                            {
-                                guidFieldInfo.SetValue(importedComponent, Guid.NewGuid().ToString());
-                                EditorUtility.SetDirty(componentWithSameGuid);
-                            }
-                        }
+                        uidFieldInfo.SetValue(objectWithSameGuid, Guid.NewGuid().ToString());
+                        EditorUtility.SetDirty(objectWithSameGuid);
                     }
                 }
             }
         }
-        
-        public static void HandleScriptableObjectDirectory<T, TDirectory>(string[] importedAssets, string directoryPath, string arrayFieldName) 
-            where T : Object 
+
+        public static void HandleScriptableObjectDirectory<T, TDirectory>(string[] importedAssets, string directoryPath, string arrayFieldName)
+            where T : Object
             where TDirectory : ScriptableObject
         {
-            Type type = typeof(T);
+            HandleScriptableObjectDirectory<T, T, TDirectory>(importedAssets, directoryPath, arrayFieldName);
+        }
+
+        public static void HandleScriptableObjectDirectory<TSource, TResult, TDirectory>(string[] importedAssets, string directoryPath, string arrayFieldName, Func<TSource, TResult> converter = null) 
+            where TSource : Object 
+            where TResult : Object 
+            where TDirectory : ScriptableObject
+        {
+            Type type = typeof(TSource);
             Type directoryType = typeof(TDirectory);
             
             FieldInfo arrayFieldInfo = directoryType.GetField(arrayFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
             
             var directory = AssetHelper.LoadOrCreateAssetAtPath<TDirectory>(directoryPath);
 
-            T[] array = (T[]) arrayFieldInfo.GetValue(directory);
-            List<T> list = array?.ToList();
+            TResult[] array = (TResult[]) arrayFieldInfo.GetValue(directory);
+            List<TResult> list = array?.ToList();
 
             if (list == null)
             {
-                list = new List<T>();
+                list = new List<TResult>();
             }
             else
             {
-                list.RemoveAll(e => e == null);
+                list.RemoveAll(e => e.Equals(default(TResult)));
             }
             
             for (int i = 0; i < importedAssets.Length; i++)
             {
                 if (type.IsAssignableFrom(AssetDatabase.GetMainAssetTypeAtPath(importedAssets[i])))
                 {
-                    var importedObject = AssetDatabase.LoadAssetAtPath<T>(importedAssets[i]);
+                    var importedObject = AssetDatabase.LoadAssetAtPath<TSource>(importedAssets[i]);
 
-                    if (!list.Contains(importedObject))
+                    var objToAdd = importedObject as TResult;
+                    
+                    if (converter != null)
                     {
-                        list.Add(importedObject);
+                        objToAdd = converter.Invoke(importedObject);
+                    }
+                    
+                    if (!list.Contains(objToAdd))
+                    {
+                        list.Add(objToAdd);
                     }
                 }
             }
@@ -172,7 +148,14 @@ namespace Rehawk.Foundation.Assets
         }
         
         public static void HandleComponentDirectory<T, TDirectory>(string[] importedAssets, string directoryPath, string arrayFieldName) 
-            where T : Component 
+            where T : Component
+            where TDirectory : ScriptableObject
+        {
+            HandleComponentDirectory<T, T, TDirectory>(importedAssets, directoryPath, arrayFieldName);
+        }
+        
+        public static void HandleComponentDirectory<TSource, TResult, TDirectory>(string[] importedAssets, string directoryPath, string arrayFieldName, Func<TSource, TResult> converter = null) 
+            where TSource : Component 
             where TDirectory : ScriptableObject
         {
             Type directoryType = typeof(TDirectory);
@@ -181,26 +164,38 @@ namespace Rehawk.Foundation.Assets
 
             var directory = AssetHelper.LoadOrCreateAssetAtPath<TDirectory>(directoryPath);
 
-            T[] array = (T[]) arrayFieldInfo.GetValue(directory);
-            List<T> list = array?.ToList();
+            TResult[] array = (TResult[]) arrayFieldInfo.GetValue(directory);
+            List<TResult> list = array?.ToList();
 
             if (list == null)
             {
-                list = new List<T>();
+                list = new List<TResult>();
             }
             else
             {
-                list.RemoveAll(e => e == null);
+                list.RemoveAll(e => e.Equals(default(TResult)));
             }
             
             for (int i = 0; i < importedAssets.Length; i++)
             {
-                var entityObject = AssetDatabase.LoadAssetAtPath<GameObject>(importedAssets[i]);
-                if (entityObject && entityObject.TryGetComponent(out T component))
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(importedAssets[i]);
+                if (prefab && prefab.TryGetComponent(out TSource component))
                 {
-                    if (!list.Contains(component))
+                    TResult objToAdd = default;
+
+                    if (component is TResult result)
                     {
-                        list.Add(component);
+                        objToAdd = result;
+                    }
+                    
+                    if (converter != null)
+                    {
+                        objToAdd = converter.Invoke(component);
+                    }
+
+                    if (!list.Contains(objToAdd))
+                    {
+                        list.Add(objToAdd);
                     }
                 }
             }

16e14ce191785baab51b7af9e8cfe504020e63bb
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2025-01-20 12:25:37 +0100

    Added AddSceneToBuild utility function

---
+++ Editor/Misc/EditorUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Rehawk.Foundation.Misc
+{
+    public static class EditorUtils
+    {
+        /// <summary>
+        /// Adds the scene with the given path to build settings as enabled.
+        /// </summary>
+        public static void AddSceneToBuild(string scenePath)
+        {
+            var tempScenes = EditorBuildSettings.scenes.ToList();
+            tempScenes.Add(new EditorBuildSettingsScene(scenePath, true));
+            EditorBuildSettings.scenes = tempScenes.ToArray();
+        }
+    }
+}

2db27d89077d4a4ecc046dadcf96cec5e7f85aea
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2025-01-28 13:19:44 +0100

    Added LayerMask extensions

---
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Rehawk.Foundation.Extensions
+{
+    public static class LayerMaskExtensions
+    {
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return mask == (mask | (1 << layer));
+        }
+    }
+}

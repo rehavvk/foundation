ffd7012c839123ec7d1d2d71e7d9811b3a8f0f1c
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2025-02-24 18:49:50 +0100

    Added GetRandom extensions for IEnumerable<T>

--- Runtime/Extensions/IEnumerableExtensions.cs
+++ Runtime/Extensions/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace Rehawk.Foundation.Extensions
 {
@@ -23,6 +24,18 @@ namespace Rehawk.Foundation.Extensions
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
         {
             return enumerable == null || !enumerable.Any();
+        }
+        
+        public static T GetRandom<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.OrderBy(_ => Random.value)
+                             .FirstOrDefault();
+        }
+        
+        public static T GetRandom<T>(this IEnumerable<T> enumerable, System.Random random)
+        {
+            return enumerable.OrderBy(_ => random.Next())
+                             .FirstOrDefault();
         }
     }
 }

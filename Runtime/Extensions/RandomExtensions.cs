b91feb7d5f75600554b5d6bb2316c7dccc5e625e
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2025-02-24 14:01:06 +0100

    Added more random extension methods

--- Runtime/Extensions/RandomExtensions.cs
+++ Runtime/Extensions/RandomExtensions.cs
@@ -9,10 +9,25 @@ namespace Rehawk.Foundation.Extensions
         {
             return (float)random.NextDouble() * (max - min) + min;
         }
-
-        public static Vector3 GetNormalizedDirection(this System.Random random)
+        
+        public static bool NextBool(this System.Random random)
         {
-            return random.GetRandomVector(-1f, 1f).normalized;
+            return random.Next(0, 2) == 0;
+        }
+
+        public static int GetRandomSign(this System.Random random)
+        {
+            return (int)Mathf.Sign(random.Next(-1, 1));
+        }
+
+        public static Vector3 GetNormalizedDirection3(this System.Random random)
+        {
+            return random.GetRandomVector3(-1f, 1f).normalized;
+        }
+
+        public static Vector2 GetNormalizedDirection2(this System.Random random)
+        {
+            return random.GetRandomVector2(-1f, 1f).normalized;
         }
 
         public static Quaternion GetRandomRotation(this System.Random random, Vector3 minEuler, Vector3 maxEuler)
@@ -22,33 +37,48 @@ namespace Rehawk.Foundation.Extensions
 
         public static Vector3 GetRandomEuler(this System.Random random, Vector3 minEuler, Vector3 maxEuler)
         {
-            return random.GetRandomVector(minEuler, maxEuler);
+            return random.GetRandomVector3(minEuler, maxEuler);
         }
 
-        public static Vector3 GetRandomVector(this System.Random random, Vector3 minVector, Vector3 maxVector)
+        public static Vector3 GetRandomVector3(this System.Random random, Vector3 minVector, Vector3 maxVector)
         {
             return new Vector3(random.NextFloat(minVector.x, maxVector.x), random.NextFloat(minVector.y, maxVector.y), random.NextFloat(minVector.z, maxVector.z));
         }
 
-        public static int GetRandomSign(this System.Random random)
-        {
-            return (int)Mathf.Sign(random.Next(-1, 1));
-        }
-
-        public static Vector3 GetRandomVector(this System.Random random, float min, float max)
+        public static Vector3 GetRandomVector3(this System.Random random, float min, float max)
         {
             return new Vector3(random.NextFloat(min, max), random.NextFloat(min, max), random.NextFloat(min, max));
         }
-        
+
+        public static Vector2 GetRandomVector2(this System.Random random, Vector2 minVector, Vector2 maxVector)
+        {
+            return new Vector2(random.NextFloat(minVector.x, maxVector.x), random.NextFloat(minVector.y, maxVector.y));
+        }
+
+        public static Vector2 GetRandomVector2(this System.Random random, float min, float max)
+        {
+            return new Vector2(random.NextFloat(min, max), random.NextFloat(min, max));
+        }
+
         public static Vector3 GetRandomPointInUnitSphere(this System.Random random)
         {
             double u = random.NextDouble();
             //Inverse of sphere volume growth curve (3/4PI * r^3)
-            double w = Math.Pow(u, (double)1 / 3);
+            double w = Math.Pow(u, (double) 1 / 3);
 
-            Vector3 direction = GetNormalizedDirection(random);
+            Vector3 direction = GetNormalizedDirection3(random);
 
-            return direction * (float)w;
+            return direction * (float) w;
+        }
+        
+        public static Vector2 GetRandomPointInUnitCircle(this System.Random random)
+        {
+            double u = random.NextDouble();
+            double w = Math.Sqrt(u);
+
+            Vector3 direction = GetNormalizedDirection2(random);
+
+            return direction * (float) w;
         }
     }
 }

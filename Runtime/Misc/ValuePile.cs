dca5b136c7e6b5b0bdda030d0d965f3bebba9bdf
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2025-02-27 16:32:34 +0100

    Added value pile data structure

---
+++ Runtime/Misc/ValuePile.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rehawk.Foundation.Misc
+{
+    public class ValuePile<T>
+    {
+        private readonly List<object> keys = new List<object>();
+        private readonly List<T> values = new List<T>();
+        private readonly T defaultValue;
+        
+        public event Action TopValueChanged;
+
+        public ValuePile(T defaultValue = default)
+        {
+            this.defaultValue = defaultValue;
+        }
+        
+        public T TopValue
+        {
+            get { return values.Count > 0 ? values[0] : defaultValue; }
+        }
+
+        public int Count
+        {
+            get { return keys.Count; }
+        }
+
+        public void Push(object key, T value)
+        {
+            T previousValue = TopValue;
+
+            int index = keys.IndexOf(key);
+
+            if (index >= 0)
+            {
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+            }
+            
+            keys.Insert(0, key);
+            values.Insert(0, value);
+            
+            if (previousValue == null || !previousValue.Equals(TopValue))
+            {
+                TopValueChanged?.Invoke();
+            }
+        }
+
+        public void Remove(object key)
+        {
+            int index = keys.IndexOf(key);
+
+            if (index >= 0)
+            {
+                T previousValue = TopValue;
+
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+            
+                if (previousValue == null || !previousValue.Equals(TopValue))
+                {
+                    TopValueChanged?.Invoke();
+                }
+            }
+        }
+
+        public bool Remove(object key, T value)
+        {
+            int index = keys.IndexOf(key);
+
+            if (index >= 0 && values[index].Equals(value))
+            {
+                T previousValue = TopValue;
+
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+            
+                if (previousValue == null || !previousValue.Equals(TopValue))
+                {
+                    TopValueChanged?.Invoke();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            T previousValue = TopValue;
+
+            keys.Clear();
+            values.Clear();
+            
+            if (previousValue == null || !previousValue.Equals(TopValue))
+            {
+                TopValueChanged?.Invoke();
+            }
+        }
+
+        public static implicit operator T(ValuePile<T> valuePile)
+        {
+            return valuePile.TopValue;
+        }
+
+        public override string ToString()
+        {
+            return TopValue.ToString();
+        }
+    }
+}
